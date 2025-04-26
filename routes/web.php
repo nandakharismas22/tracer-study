81b9977012a6640f3db8fd7ba210dd656d0512d7
AryaRizky <aryarizkytriputra230304@gmail.com>
2025-04-26 18:56:11 +0700

    :recycle: refactor: clear path

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LandingPageController;
 use App\Imports\AlumniImport;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingPageController::class, 'index']);
@@ -18,4 +19,11 @@ Route::get('linear', function () {
     Excel::import(new AlumniImport, $filePath);
 
     return 'Import completed.';
+});
+
+Route::get('clear', function(){
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
 });
